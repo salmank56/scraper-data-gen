b1ab60e9faa17fe9fd414ebaeb71e8c76bce8551
Imran <imran@resonance.holdings>
2024-08-25 17:07:55 -0400

    error page

--- src/main.tsx
+++ src/main.tsx
@@ -10,13 +10,17 @@ import {
 } from "react-router-dom";
 import Home from "./pages/Home.tsx";
 import EditPrompts from "./pages/EditPrompts.tsx";
+import { Error } from "./pages/Error.tsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" Component={App}>
-      <Route index path="/" Component={Home} />
-      <Route index path="/edit-prompts" Component={EditPrompts} />
-    </Route>
+    <>
+      <Route path="/" Component={App} errorElement={<Error/>}>
+        <Route index path="/" Component={Home} />
+        <Route index path="/edit-prompts" Component={EditPrompts} />
+      </Route>
+      <Route path="/error" element={<Error/>}></Route>
+    </>
   )
 );
 
