76cef6b5d4b3147beb552fd8cc1438662f7449c0
Imran <imran@resonance.holdings>
2024-08-25 17:41:23 -0400

    error page

--- src/config.ts
+++ src/config.ts
@@ -5,5 +5,5 @@ export const WINDOWS_SESSION_REQUEST = Object.freeze({request: "getToken"});
 export const WINDOWS_VALID_ORIGINS = Object.freeze([
     "http://localhost:3000",
     "https://app.thequantuminsider.com",
-    "https://dev-app.thequantuminsider.com/"
+    "https://dev-app.thequantuminsider.com"
 ]);
